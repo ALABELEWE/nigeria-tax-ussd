53291c281c1857b7a819bb0699ad0c244a20f1de
ALABELEWE <ridwan.osein@int.softalliance.com>
2026-02-19 10:32:06 +0100

    Add sender-id to africastalking config

--- src/main/java/com/taxhelp/nigerian_tax_ussd/config/AfricasTalkingProperties.java
+++ src/main/java/com/taxhelp/nigerian_tax_ussd/config/AfricasTalkingProperties.java
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "africastalking")
 public class AfricasTalkingProperties {
-    private String username = "sandbox";
-    private String apiKey = "atsk_b7393a0766a5ff7ef60d85dc00dbfd098554cb45178b65465a9e4274f14327753a2fb888";
-    private String smsUrl = "https://api.sandbox.africastalking.com/version1/messaging";
-    private String shortcode ="*384*36903#";
-    private String senderId = "12096";
-
+    private String username;
+    private String apiKey;
+    private String smsUrl;
+    private String shortcode;
+    private String senderId;
 }
